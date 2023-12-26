fee510e1bd80dd30bcb1698f2a1a2732fcb8159e
Eduardo De Patta <eduardo.depatta@gmail.com>
2023-12-25 22:46:52 -0300

    feat: create SugnUp view

--- src/views/auth/SignUp.tsx
+++ src/views/auth/SignUp.tsx
@@ -1,47 +1,74 @@
-import { FC, useState } from 'react'
-import { Button, SafeAreaView, StyleSheet, View } from 'react-native'
+import { FC } from 'react'
+import { SafeAreaView, StyleSheet, View } from 'react-native'
 import colors from '../../constants/colors'
-import AuthInputField from '../../components/AuthInputField'
+import AuthInputField from '../../components/form/AuthInputField'
+import * as yup from 'yup'
+import Form from '../../components/form'
+import SubmitButton from '../../components/form/SubmitBtn'
+
+const initialValue = {
+  name: '',
+  email: '',
+  password: '',
+}
+
+const signUpSchema = yup.object({
+  name: yup
+    .string()
+    .trim('Name is missing!')
+    .min(3, 'Invalid name!')
+    .required('Name is required!'),
+  email: yup
+    .string()
+    .trim('Email is missing!')
+    .email('Invalid email!')
+    .required('Email is required!'),
+  password: yup
+    .string()
+    .trim('Password is missing!')
+    .min(8, 'Password is too short!')
+    .matches(
+      /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#\$%\^&\*])[a-zA-Z\d!@#\$%\^&\*]+$/,
+      'Password is too simple!'
+    )
+    .required('Email is required!'),
+})
 
 const SignUp: FC = () => {
-  const [userInfo, setUserInfo] = useState({
-    name: '',
-    email: '',
-    password: '',
-  })
-
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.formContainer}>
-        <AuthInputField
-          placeholder='Enter your name'
-          label='Name'
-          containerStyle={styles.marginBottom}
-          onChangeText={(name) => setUserInfo({ ...userInfo, name })}
-        />
-        <AuthInputField
-          keyboardType='email-address'
-          placeholder='Enter your email'
-          label='Email'
-          autoCapitalize='none'
-          containerStyle={styles.marginBottom}
-          onChangeText={(email) => setUserInfo({ ...userInfo, email })}
-        />
-        <AuthInputField
-          autoCapitalize='none'
-          secureTextEntry
-          placeholder='********'
-          label='Password'
-          onChangeText={(password) => setUserInfo({ ...userInfo, password })}
-          containerStyle={styles.marginBottom}
-        />
-        <Button
-          title='Sign up'
-          onPress={() => {
-            console.log(userInfo)
-          }}
-        />
-      </View>
+      <Form
+        initialValues={initialValue}
+        validationSchema={signUpSchema}
+        onSubmit={(values) => {
+          console.log(values)
+        }}
+      >
+        <View style={styles.formContainer}>
+          <AuthInputField
+            placeholder='Enter your name'
+            label='Name'
+            name='name'
+            containerStyle={styles.marginBottom}
+          />
+          <AuthInputField
+            keyboardType='email-address'
+            placeholder='Enter your email'
+            label='Email'
+            name='email'
+            autoCapitalize='none'
+            containerStyle={styles.marginBottom}
+          />
+          <AuthInputField
+            autoCapitalize='none'
+            secureTextEntry
+            name='password'
+            placeholder='********'
+            label='Password'
+          />
+          <SubmitButton title='Sign up' />
+        </View>
+      </Form>
     </SafeAreaView>
   )
 }
