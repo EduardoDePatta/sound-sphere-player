fb8cac24ad66f41cacd282e86dd04c617815b6f4
Eduardo De Patta <eduardo.depatta@gmail.com>
2023-12-25 22:48:13 -0300

    refactor: handle error messages with formik context

--- src/components/form/AuthInputField.tsx
+++ src/components/form/AuthInputField.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 import {
   StyleProp,
   StyleSheet,
@@ -7,39 +7,52 @@ import {
   View,
   ViewStyle,
 } from 'react-native'
-import AppInput from '../ui/AppInput'
-import colors from '../constants/colors'
+import AppInput from '../../ui/AppInput'
+import colors from '../../constants/colors'
+import { useFormikContext } from 'formik'
 
 interface AuthInputFieldProps {
+  name: string
   label?: string
   placeholder?: string
   keyboardType?: TextInputProps['keyboardType']
   autoCapitalize?: TextInputProps['autoCapitalize']
   secureTextEntry?: TextInputProps['secureTextEntry']
   containerStyle?: StyleProp<ViewStyle>
-  onChangeText?: (text: string) => void
 }
 
 const AuthInputField: FC<AuthInputFieldProps> = (props) => {
+  const { handleChange, values, errors, handleBlur, touched } =
+    useFormikContext<{
+      [key: string]: string
+    }>()
+
   const {
     label,
+    name,
     placeholder,
     keyboardType,
     autoCapitalize,
     secureTextEntry,
     containerStyle,
-    onChangeText,
   } = props
+
+  const errorMessage = touched[name] && errors[name] ? errors[name] : ''
 
   return (
     <View style={[styles.container, containerStyle]}>
-      <Text style={styles.label}>{label}</Text>
+      <View style={styles.labelContainer}>
+        <Text style={styles.label}>{label}</Text>
+        <Text style={styles.errorMessage}>{errorMessage}</Text>
+      </View>
       <AppInput
         placeholder={placeholder}
         keyboardType={keyboardType}
         autoCapitalize={autoCapitalize}
         secureTextEntry={secureTextEntry}
-        onChangeText={onChangeText}
+        onChangeText={handleChange(name)}
+        value={values[name]}
+        onBlur={handleBlur(name)}
       />
     </View>
   )
@@ -49,8 +62,15 @@ const styles = StyleSheet.create({
   container: {},
   label: {
     color: colors.CONTRAST,
-    paddingLeft: 10,
-    marginBottom: 5,
+  },
+  errorMessage: {
+    color: colors.ERROR,
+  },
+  labelContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 5,
   },
 })
 
