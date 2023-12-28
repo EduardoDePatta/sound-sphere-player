1ba996da427b08b22718e44d7fb29514b1765c01
Eduardo De Patta <eduardo.depatta@gmail.com>
2023-12-28 18:57:14 -0300

    feat: add loading state controller to auth store

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -14,11 +14,13 @@ export interface UserProfile {
 interface AuthState {
   profile: UserProfile | null
   loggedIn: boolean
+  loading: boolean
 }
 
 const initialState: AuthState = {
   profile: null,
   loggedIn: false,
+  loading: false,
 }
 
 const slice = createSlice({
@@ -31,10 +33,14 @@ const slice = createSlice({
     updateLoggedInState(authState, { payload }: PayloadAction<boolean>) {
       authState.loggedIn = payload
     },
+    updateLoadingState(authState, { payload }: PayloadAction<boolean>) {
+      authState.loading = payload
+    },
   },
 })
 
-export const { updateLoggedInState, updateProfile } = slice.actions
+export const { updateLoggedInState, updateProfile, updateLoadingState } =
+  slice.actions
 
 export const getAuthState = createSelector(
   (state: RootState) => state,
