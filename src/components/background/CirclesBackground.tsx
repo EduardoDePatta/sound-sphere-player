aa1a114e8db2f4ab86ef180dedb369950bef3d47
Eduardo De Patta <eduardo.depatta@gmail.com>
2023-12-27 22:40:33 -0300

    feat: create background component

---
+++ src/components/background/CirclesBackground.tsx
@@ -0,0 +1,43 @@
+import { FC } from 'react'
+import { View } from 'react-native'
+import CircleUI from '../../ui/CircleUI'
+import colors from '../../constants/colors'
+
+const CirclesBackground: FC = () => {
+  return (
+    <>
+      <CircleUI
+        innerColor={colors.SECONDARY}
+        outerColor={colors.SECONDARY}
+        size={80}
+        position={{ top: 15, left: 15 }}
+      />
+      <CircleUI
+        innerColor={colors.SECONDARY}
+        outerColor={colors.SECONDARY}
+        size={40}
+        position={{ top: 120, left: 120 }}
+      />
+      <CircleUI
+        innerColor={colors.SECONDARY}
+        outerColor={colors.SECONDARY}
+        size={120}
+        position={{ top: 80, left: 336 }}
+      />
+      <CircleUI
+        innerColor={colors.SECONDARY}
+        outerColor={colors.SECONDARY}
+        size={160}
+        position={{ top: 500, left: 120 }}
+      />
+      <CircleUI
+        innerColor={colors.SECONDARY}
+        outerColor={colors.SECONDARY}
+        size={160}
+        position={{ top: 760, left: -40 }}
+      />
+    </>
+  )
+}
+
+export default CirclesBackground
