6d8e607dc5f1d1287ba15ba9ee2f6321935a01b8
Eduardo De Patta <eduardo.depatta@gmail.com>
2023-12-27 22:40:14 -0300

    style: change base colors

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,9 +1,10 @@
 export default {
-  PRIMARY: '#443333',
-  SECONDARY: '#EEA849',
-  CONTRAST: '#FFFFFF',
+  PRIMARY: '#3A4B5C',
+  SECONDARY: '#3e88b3',
+  CONTRAST: '#D8E1DF',
   INACTIVE_CONTRAST: 'rgba(255, 255, 255, 0.5)',
-  ERROR: '#FF5B14',
+  CONTRAST_2: '#7ebfe6',
+  ERROR: '#bd4a67',
   SUCCESS: '#3BEF1E',
   OVERLAY: 'rgba(45, 35, 35, 0.5)',
 }
