45ee236ef323e49d008b868751ec71b40b8362ed
Eduardo De Patta <eduardo.depatta@gmail.com>
2023-12-28 18:58:39 -0300

    feat: pass default theme to appnavigator

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,31 +1,47 @@
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import { FC, useCallback, useEffect } from 'react'
-import AuthNavigator from './AuthNavigator'
 import { useDispatch, useSelector } from 'react-redux'
-import { getAuthState, updateProfile } from '../store/auth'
+import AuthNavigator from './AuthNavigator'
+import { getAuthState, updateLoadingState, updateProfile } from '../store/auth'
 import TabNavigator from './TabNavigator'
 import { Keys, getFromAsyncStorage } from '../storage/asyncStorage'
 import client from '../api/client'
+import { StyleSheet, View } from 'react-native'
+import Spinner from 'react-native-loading-spinner-overlay'
+import colors from '../constants/colors'
+import Loader from '../ui/Loader'
 
 interface indexProps {}
 
+const AppTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: colors.PRIMARY,
+    primary: colors.CONTRAST_2,
+  },
+}
+
 const AppNavigator: FC<indexProps> = () => {
-  const { loggedIn } = useSelector(getAuthState)
+  const { loggedIn, loading } = useSelector(getAuthState)
   const dispatch = useDispatch()
 
   const fetchAuthInfo = useCallback(async () => {
     try {
+      dispatch(updateLoadingState(true))
       const token = await getFromAsyncStorage(Keys.AUTH_TOKEN)
       if (!token) return
       const { data } = await client.get('/auth/is-auth', {
         headers: {
-          Authorization: 'Bearer ' + token,
+          Authorization: `Bearer ${token}`,
         },
       })
       dispatch(updateProfile(data.profile))
       console.log('🚀 ~ file: index.tsx:24 ~ fetchAuthInfo ~ data:', data)
     } catch (error) {
       console.log('auth errorrrrrr bla bla: ', error)
+    } finally {
+      dispatch(updateLoadingState(false))
     }
   }, [dispatch])
 
@@ -34,10 +50,17 @@ const AppNavigator: FC<indexProps> = () => {
   }, [fetchAuthInfo])
 
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={AppTheme}>
+      {loading ? <Loader loading={loading} /> : null}
       {loggedIn ? <TabNavigator /> : <AuthNavigator />}
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  spinnerTextStyle: {
+    color: colors.CONTRAST,
+  },
+})
 
 export default AppNavigator
