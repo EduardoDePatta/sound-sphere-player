6efedd8e7fddf07cfc4906d577e3a0ff2d755eb9
Eduardo De Patta <eduardo.depatta@gmail.com>
2023-12-28 18:56:46 -0300

    style: add icons to tabs

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,18 +1,57 @@
-import { FC } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Home from '../views/Home'
 import Profile from '../views/Profile'
 import Upload from '../views/Upload'
+import colors from '../constants/colors'
+import { AntDesign, MaterialCommunityIcons } from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator()
 
-const TabNavigator = () => {
+function TabNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name='HomeScreen' component={Home} />
-      <Tab.Screen name='ProfileScreen' component={Profile} />
-      <Tab.Screen name='UploadScreen' component={Upload} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: { backgroundColor: colors.PRIMARY },
+      }}
+    >
+      <Tab.Screen
+        name='HomeScreen'
+        component={Home}
+        options={{
+          tabBarIcon: ({ size, color }) => {
+            return <AntDesign name='home' size={size} color={color} />
+          },
+          tabBarLabel: 'Home',
+        }}
+      />
+      <Tab.Screen
+        name='ProfileScreen'
+        component={Profile}
+        options={{
+          tabBarIcon: ({ size, color }) => {
+            return <AntDesign name='user' size={size} color={color} />
+          },
+          tabBarLabel: 'Profile',
+        }}
+      />
+      <Tab.Screen
+        name='UploadScreen'
+        component={Upload}
+        options={{
+          tabBarIcon: ({ size, color }) => {
+            return (
+              <MaterialCommunityIcons
+                name='account-music-outline'
+                size={size}
+                color={color}
+              />
+            )
+          },
+          tabBarLabel: 'Upload',
+        }}
+      />
     </Tab.Navigator>
   )
 }
