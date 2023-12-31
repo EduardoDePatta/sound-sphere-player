01f2ab1c27cb134ca7d3a4b8014e14c261cdab71
Eduardo De Patta <eduardo.depatta@gmail.com>
2023-12-30 22:12:55 -0300

    feat: add lastuploads component to home view

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,18 +1,25 @@
 import { FC } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import { useFetchLatestAudios } from '../hooks/query'
+import Loader from '../ui/Loader'
+import LatestUploads from '../components/LatestUploads'
+import RecommendedAudios from '../components/RecommendedAudios'
 
 interface HomeProps {}
 
-const Home: FC<HomeProps> = (props) => {
+const Home: FC<HomeProps> = () => {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <LatestUploads />
+      <RecommendedAudios />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    padding: 10,
+  },
 })
 
 export default Home
