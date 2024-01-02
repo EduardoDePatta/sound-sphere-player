25568d85e3943b63c1b45c6bcc4da46095e7b1cd
Eduardo De Patta <eduardo.depatta@gmail.com>
2024-01-01 23:52:20 -0300

    feat: create audio list item component

--- src/ui/AudioListItem.tsx
+++ src/ui/AudioListItem.tsx
@@ -1,46 +1,83 @@
 import { FC } from 'react'
-import { Image, Pressable, ScrollView, StyleSheet, Text } from 'react-native'
+import {
+  Image,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
 import colors from '../constants/colors'
 import { AudioData } from '../@types/audio'
+import { Skeleton } from 'moti/skeleton'
 
 interface AudioListItemProps {
   audio: AudioData
   onPress?: () => void
+  loading: boolean
 }
 
-const AudioListItem: FC<AudioListItemProps> = ({ audio, onPress }) => {
+const SkeletonCommonProps = {
+  colorMode: 'light',
+  transition: {
+    type: 'timing',
+    duration: 2000,
+  },
+  backgroundColor: colors.OVERLAY,
+} as const
+
+const AudioListItem: FC<AudioListItemProps> = ({ audio, onPress, loading }) => {
   const getSource = (poster?: string) => {
     return poster ? { uri: poster } : require('../assets/music_small.png')
   }
   return (
-    <Pressable onPress={onPress} style={styles.listItem}>
-      <Image style={styles.poster} source={getSource(audio.poster)} />
-      <ScrollView style={styles.titleContainer}>
-        <Text style={styles.title} numberOfLines={1} ellipsizeMode='tail'>
-          {audio.title}
-        </Text>
-        <Text style={styles.owner} numberOfLines={1} ellipsizeMode='tail'>
-          {audio.owner.name}
-        </Text>
-      </ScrollView>
-    </Pressable>
+    <>
+      <Pressable onPress={onPress} style={styles.listItem}>
+        <Skeleton
+          show={loading}
+          height={50}
+          width={50}
+          radius={5}
+          {...SkeletonCommonProps}
+        >
+          <Image style={styles.poster} source={getSource(audio.poster)} />
+        </Skeleton>
+        <ScrollView style={styles.titleContainer}>
+          <View>
+            <Skeleton show={loading} height={20} {...SkeletonCommonProps}>
+              <Text style={styles.title} numberOfLines={2} ellipsizeMode='tail'>
+                {audio.title}
+              </Text>
+            </Skeleton>
+            <Skeleton show={loading} height={20} {...SkeletonCommonProps}>
+              <Text style={styles.owner} numberOfLines={1} ellipsizeMode='tail'>
+                {audio.owner.name}
+              </Text>
+            </Skeleton>
+          </View>
+        </ScrollView>
+      </Pressable>
+    </>
   )
 }
 
 const styles = StyleSheet.create({
   listItem: {
     flexDirection: 'row',
-    backgroundColor: colors.OVERLAY,
     marginBottom: 15,
     borderRadius: 5,
+    gap: 10,
   },
   titleContainer: {
     flex: 1,
     padding: 5,
+    backgroundColor: colors.OVERLAY,
+    borderRadius: 5,
   },
   poster: {
     width: 50,
     height: 50,
+    borderRadius: 5,
   },
   title: {
     color: colors.CONTRAST,
