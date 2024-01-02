e33affa8865951115e6978ab8e587d1fa0516395
Eduardo De Patta <eduardo.depatta@gmail.com>
2024-01-02 03:01:44 -0300

    feat: add soundReducer to combinereducers

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import authReducer from './auth'
 import notificationReducer from './notification'
+import soundReducer from './sound'
 
 const reducer = combineReducers({
   auth: authReducer,
   notification: notificationReducer,
+  sound: soundReducer,
 })
 
 const store = configureStore({
